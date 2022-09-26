841a0767bb4806586ad9b3d43b119e4b1d450b7d
Anna44856 <44856@edu.raseko.fi>
2022-09-26 10:06:31 +0300

    Päivitä modeleita

--- varauskalenteri/models.py
+++ varauskalenteri/models.py
@@ -1,40 +1,49 @@
 import datetime
+from time import timezone
 
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
+
 
 
 
 class Tapahtuma(models.Model):
     otsikko = models.CharField(max_length=200)
-    kuvaus = models.TextField()
+    kuvaus = models.TextField(blank=True)
     alku = models.DateTimeField()
-    loppu = models.DateTimeField()
+    loppu = models.DateTimeField(null=True, blank=True)
     osallistujat = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
-        required=False,
-        #blank=True,
+        blank=True,
         )
     paikkoja = models.IntegerField()
 
-    def __str__(self):
-        return f"{self.otsikko} ({self.alku}--{self.loppu})"
+def __str__(self):
+    alku = timezone.localtime(self.alku)
+    # Ternary operator - kolmiosainen opperaattori
+    # A if EHTO else B
+    # Jos ehto on tosi, niin tulos on A
+    # Jos ehto on epätosi, niin tulos on B
+    loppu = timezone.localtime(self.loppu) if self.loppu else None
+    loppu_teksti = f"{loppu:%d.%m.%Y %H:%M}" if loppu else ""
+    return f"{self.otsikko} ({alku:%d.%m.%Y %H:%M} -- {loppu_teksti})"
 
-    def kesto(self) -> datetime.timedelta:
-        return self.loppu - self.alku
+def kesto(self) -> datetime.timedelta:
+    return self.loppu - self.alku
 
-    def kesto_tuntia(self):
-        kesto = self.kesto()
-        return kesto.total_seconds() / 3600
+def kesto_tuntia(self):
+    kesto = self.kesto()
+    return kesto.total_seconds() / 3600
 
-    def varaa(self, user):
-        if user in self.osallistujat.all():
-            return True
-        osallistujia = self.osallistujat.all().count()
-        if osallistujia + 1 > self.paikkoja:
-            return False
-        self.osallistujat.add(user)
+def varaa(self, user):
+    if user in self.osallistujat.all():
         return True
+    osallistujia = self.osallistujat.all().count()
+    if osallistujia + 1 > self.paikkoja:
+        return False
+    self.osallistujat.add(user)
+    return True
 
-    def onko_varattu(self, user):
-        return (user in self.osallistujat.all())
+def onko_varattu(self, user):
+    return (user in self.osallistujat.all())
